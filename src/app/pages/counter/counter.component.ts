9a91dfb4c88a15239f84e1f41bf18d9942c97d5d
Wladimir Guevara Berbesi <wladige@gmail.com>
2025-03-28 14:15:17 -0300

    ejemplo básico de signals

--- src/app/pages/counter/counter.component.ts
+++ src/app/pages/counter/counter.component.ts
@@ -1,21 +1,32 @@
-import { Component, signal } from "@angular/core";
+import { Component, signal, ChangeDetectionStrategy } from "@angular/core";
 
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-counter',
     templateUrl: './counter.component.html',
+    styles: `
+    button {
+        margin: 5px;
+        width: 100px;
+    }`,
 })
 export class CounterComponent {
+    counterSignal= signal(5);
     counter = 10;
     sumar(value: number) {
         // this.counter++;
         this.counter += value;
+        // this.counterSignal.set(this.counterSignal() + value); a pesar que funciona, no se debe ocupar el .set, sino el .update
+        this.counterSignal.update(current => current + value);
     }
     restar(value: number) {
         if(this.counter === 0) return;
         this.counter -= value;
+        this.counterSignal.update(current => current - value);
     }
     reset() {    
         this.counter = 0;
+        this.counterSignal.set(0);
     }
 };
