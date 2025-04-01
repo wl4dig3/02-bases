706cdabf9508dd9f830bccd582a5a206030fe268
Wladimir Guevara Berbesi <wladige@gmail.com>
2025-04-01 10:11:08 -0300

    feat: usando metodo update en signal

--- src/app/pages/dragonball/dragonball-paage.component.ts
+++ src/app/pages/dragonball/dragonball-paage.component.ts
@@ -12,10 +12,29 @@ interface Character {
   styleUrls: ['./dragonball-paage.component.css']
 })
 export class DragonballComponent {
+
+  name = signal('Iroman');
+  power = signal(4500);
+
   characters= signal<Character[]>([
-    { id: 1, name: 'Goku', power: 10001 },
-    { id: 2, name: 'Vegeta', power: 9001 },
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
     { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 }
   ]);
+  // total = signal<number[]>;
+  addCharacter(): void {
+  if (!this.name() || !this.power() || this.power() < 0) return;
+    const newCharacter: Character = { id: this.characters().length + 1, name: this.name(), power: this.power() };
+    // this.characters().push(newCharacter); esta es la manera antigua de hacerla
+    this.characters.update((list)  => [...list, newCharacter]);
+    console.log(newCharacter);
+    console.log(this.characters());
+    this.resesrFields();
+  }
+  resesrFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
 }
 
