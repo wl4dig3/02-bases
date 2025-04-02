be39e449bed981e641efaecefb8a408009bac5b1
Wladimir Guevara Berbesi <wladige@gmail.com>
2025-04-02 10:38:54 -0300

    feat: emision de evento con output

--- src/app/components/dragonball/character-add/dragonball-character-add.component.ts
+++ src/app/components/dragonball/character-add/dragonball-character-add.component.ts
@@ -1,11 +1,39 @@
-import { Component, input } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import type { Character } from '../../../interfaces/character.interface';
 
 @Component({
   selector: 'dragonball-character-add',
   templateUrl: './dragonball-character-add.component.html',
 })
+
 export class CharacterAddComponent {
-characters = input.required<Character[]>();
+
+// characters = input.required<Character[]>();
+
+name = signal('Iroman W');
+power = signal(4500);
+
+newCharacter = output<Character>();
+  addCharacter(): void {
+  if (!this.name() || !this.power() || this.power() < 0) return;
+    const newCharacter: Character = { 
+        id: Math.floor(Math.random() * 1000), 
+        name: this.name(), 
+        power: this.power() 
+    };
+    // this.characters().push(newCharacter); esta es la manera antigua de hacerla
+    // this.characters.update((list)  => [...list, newCharacter]);
+    // usando output:
+    this.newCharacter.emit(newCharacter);
+    console.log({newCharacter});
+    // console.log(this.characters());
+    this.resesrFields();
+  }
+
+  resesrFields(){
+    this.name.set('');
+    this.power.set(0);
+  
+}
 
 }
--- src/app/pages/dragonball-super/dragonball-page-super.component.ts
+++ src/app/pages/dragonball-super/dragonball-page-super.component.ts
@@ -1,37 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
 import { Character } from '../../interfaces/character.interface';
-
+import { CharacterAddComponent } from '../../components/dragonball/character-add/dragonball-character-add.component';
 
 
 @Component({
   selector: 'app-dragonball-super',
   templateUrl: './dragonball-page-super.component.html',
   styleUrls: ['./dragonball-page-super.component.css'],
-  imports: [CharacterListComponent]
+  imports: [CharacterListComponent, CharacterAddComponent]
 })
 export class DragonballPageSuperComponent {
 
-  name = signal('Iroman');
-  power = signal(4500);
 
   characters= signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
   ]);
-  // total = signal<number[]>;
-  addCharacter(): void {
-  if (!this.name() || !this.power() || this.power() < 0) return;
-    const newCharacter: Character = { id: this.characters().length + 1, name: this.name(), power: this.power() };
-    // this.characters().push(newCharacter); esta es la manera antigua de hacerla
-    // this.characters.update((list)  => [...list, newCharacter]);
-    console.log(newCharacter);
-    console.log(this.characters());
-    this.resesrFields();
-  }
-  resesrFields(){
-    this.name.set('');
-    this.power.set(0);
+  
+  addCaracter(character: Character){
+    this.characters.update(list => [...list, character]);
   }
 }
 
